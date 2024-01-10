471b435e0eb201bdbffecc41ca0c6dce24f7e380
unknown <annemari.tamm@gmail.com>
2024-01-10 11:19:33 +0200

    Issue time tracking added

--- cypress/e2e/issue-time-tracking.cy.js
+++ cypress/e2e/issue-time-tracking.cy.js
@@ -1,86 +1,138 @@
 describe("issue time tracking", () => {
-    beforeEach(() => {
-      cy.visit("/");
-      cy.url()
-        .should("eq", `${Cypress.env("baseUrl")}project/board`)
-        .then((url) => {
-          cy.visit(url + "/board");
-        });
-    });
-    it("Should add, update and remove estimation to an issue where no time is added ", () => {
-      //Open issue
-      cy.contains(existingIssueTitle).click();
-      getissueDetailsModal().should("be.visible");
-      //***Check that time tracker has no spent time added and (“No Time Logged” label is visible)
-      //cy.contains("No time logged").should("be.visible");
-      //ADD ESTIMATION
-      // Add value 10 to “Original estimate (hours)” field
-      cy.get('input[placeholder="Number"]')
-        .click()
-        .clear()
-        .type(addEstimationValue);
-      cy.contains("Original Estimate (hours)").click();
-      //Close issue detail page
-      cy.get('[data-testid="icon:close"]').first().trigger("click");
-      //Reopen the same issue to check that original estimation is saved
-      cy.contains(existingIssueTitle).click();
-      cy.contains("10h estimated").should("be.visible");
-  
-      //UPDATE ESTIMATION
-      //Update estimation value to 20
-      cy.get('input[placeholder="Number"]')
-        .click()
-        .clear()
-        .type(updateEstimationValue);
-      cy.contains("Original Estimate (hours)").click();
-  
-      //Close issue detail page
-      cy.get('[data-testid="icon:close"]').first().trigger("click");
-      //Reopen the same issue to check that original estimation is saved
-      cy.contains(existingIssueTitle).click();
-      cy.contains("20h estimated").should("be.visible");
-      //REMOVE ESTIMATION
-      //Removes value from the field “Original estimate (hours)”
-      cy.get('input[placeholder="Number"]').click().clear();
-      cy.contains("Original Estimate (hours)").click();
-      //Close issue detail page
-      cy.get('[data-testid="icon:close"]').first().trigger("click");
-      //Reopen the same issue to check that time tracker does not have estimation
-      cy.contains(existingIssueTitle).click();
-      //cy.contains("20h estimated").should("not.exsist");
-    });
-    it("..", () => {
-      //createNewIssue();
-      //    cy.get('[data-testid="board-list:backlog"]')
-      //    .should("contain", issueTitle)
-      //  .click();
-      //  //cy.contains(issueTitle).click();
-      //getissueDetailsModal().should("be.visible");
-    });
-  
-    const getissueDetailsModal = () =>
-      cy.get('[data-testid="modal:issue-details"]');
-    const existingIssueTitle = "This is an issue of type: Task.";
-    const issueTitle = "time tracking title";
-    const issueDescription = "time tracking description";
-    function createNewIssue() {
-      cy.get('[data-testid="icon:plus"]').click();
-      cy.get('[data-testid="modal:issue-create"]').within(() => {
-        cy.get('[data-testid="select:type"]').click();
-        cy.get('[data-testid="icon:task"]').trigger("click");
-        cy.get(".ql-editor").click().type(issueDescription);
-        cy.get('[name="title"]').click().type(issueTitle);
-        cy.get('[data-testid="select:userIds"]').click();
-        cy.get('[data-testid="select-option:Baby Yoda"]').trigger("click");
-        cy.get('[data-testid="select:priority"]').click();
-        cy.get('[data-testid="select-option:Low"]').trigger("click");
-        cy.get('button[type="submit"]').click();
+  beforeEach(() => {
+    cy.visit("/");
+    cy.url()
+      .should("eq", `${Cypress.env("baseUrl")}project/board`)
+      .then((url) => {
+        cy.visit(url + "/board");
+        cy.contains(issueTitle).click();
+        getissueDetailsModal().should("be.visible");
       });
-      cy.get('[data-testid="modal:issue-create"]').should("not.exist");
-      cy.contains("Issue has been successfully created.").should("be.visible");
-      cy.reload();
-      cy.contains("Issue has been successfully created.").should("not.exist");
-    }
-    const addEstimationValue = "10";
-    const updateEstimationValue = "20";
   });
+  it("Should add, update and remove time estimation successfully", () => {
+    addEstimation();
+
+    ensureAddedEstimationIsSaved();
+
+    updateEstimation();
+
+    ensureUpdatedEstimationIsSaved();
+
+    removeEstimation();
+
+    ensureRemovedEstimationIsSaved();
+  });
+  it("Should log time and remove logged time successfully", () => {
+    //Log time
+    openTimeTrackingModal();
+
+    updateTimeSpent();
+
+    updateTimeRemaining();
+
+    clickDoneButton();
+    //Check that updated spent time and time remaining are visible in the time tracking section
+    getissueDetailsModal().within(() => {
+      cy.contains("div", "2h logged").should("be.visible");
+      cy.contains("div", "5h remaining").should("be.visible");
+      cy.contains("div", "No Time Logged").should("not.exist");
+    });
+
+    //Remove logged time
+    openTimeTrackingModal();
+
+    removeTimeSpentValue();
+
+    removeTimeRemainingValue();
+
+    clickDoneButton();
+
+    //Check that original estimation is visible and no time is logged
+    getissueDetailsModal().within(() => {
+      cy.contains("div", "8h estimated").should("be.visible");
+      cy.contains("div", "No time logged").should("be.visible");
+    });
+  });
+
+  const getissueDetailsModal = () =>
+    cy.get('[data-testid="modal:issue-details"]');
+  const issueTitle = "This is an issue of type: Task.";
+  const getPlaceholderNumber = () => cy.get('input[placeholder="Number"]');
+
+  //Estimation functions
+  const addedEstimationValue = "10";
+  const addedEstimateRemaining = "10h estimated";
+  const updatedEstimationValue = "20";
+  const updatedEstimateRemaining = "20h estimated";
+  function addEstimation() {
+    getPlaceholderNumber().click().clear().type(addedEstimationValue);
+    cy.contains("Original Estimate (hours)").click();
+    cy.contains(addedEstimateRemaining).should("be.visible");
+  }
+  function ensureAddedEstimationIsSaved() {
+    cy.get('[data-testid="icon:close"]').first().trigger("click");
+    cy.contains(issueTitle).click();
+    cy.contains(addedEstimateRemaining).should("be.visible");
+  }
+  function updateEstimation() {
+    getPlaceholderNumber().click().clear().type(updatedEstimationValue);
+    cy.contains("Original Estimate (hours)").click();
+    cy.contains(updatedEstimateRemaining).should("be.visible");
+  }
+  function ensureUpdatedEstimationIsSaved() {
+    cy.get('[data-testid="icon:close"]').first().trigger("click");
+    cy.contains(issueTitle).click();
+    cy.contains(updatedEstimateRemaining).should("be.visible");
+  }
+  function removeEstimation() {
+    getPlaceholderNumber().click().clear();
+    cy.contains("Original Estimate (hours)").click();
+    cy.contains(updatedEstimateRemaining).should("not.exist");
+  }
+  function ensureRemovedEstimationIsSaved() {
+    cy.get('[data-testid="icon:close"]').first().trigger("click");
+    cy.contains(issueTitle).click();
+    cy.contains(updatedEstimateRemaining).should("not.exist");
+  }
+
+  //Time logging functions
+  const updatedTimeSpentValue = "2";
+  const updatedTimeRemainingValue = "5";
+  function openTimeTrackingModal() {
+    cy.get('[data-testid="icon:stopwatch"]').click();
+    cy.get('[data-testid="modal:tracking"]').should("be.visible");
+  }
+  function updateTimeSpent() {
+    cy.get('[data-testid="modal:tracking"]').within(() => {
+      cy.get('input[placeholder="Number"]')
+        .first()
+        .click()
+        .clear()
+        .type(updatedTimeSpentValue);
+    });
+  }
+  function updateTimeRemaining() {
+    cy.get('[data-testid="modal:tracking"]').within(() => {
+      cy.get('input[placeholder="Number"]')
+        .eq(1)
+        .click()
+        .clear()
+        .type(updatedTimeRemainingValue);
+    });
+  }
+  function removeTimeSpentValue() {
+    cy.get('[data-testid="modal:tracking"]').within(() => {
+      cy.get('input[placeholder="Number"]').first().click().clear();
+    });
+  }
+  function removeTimeRemainingValue() {
+    cy.get('[data-testid="modal:tracking"]').within(() => {
+      cy.get('input[placeholder="Number"]').eq(1).click().clear();
+    });
+  }
+  function clickDoneButton() {
+    cy.get('[data-testid="modal:tracking"]').within(() => {
+      cy.contains("Done").click();
+    });
+  }
+});
